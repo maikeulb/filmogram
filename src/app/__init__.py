5be275cf8dbf55c34f1e9fd466014456abfe6015
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-05 13:03:05 -0500

    add ajax for like feature

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -52,7 +52,7 @@ def register_extensions(app):
 def register_blueprints(app):
     app.register_blueprint(account_bp, url_prefix='/account')
     app.register_blueprint(main_bp)
-    app.register_blueprint(api_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
     return None
 
 def register_errorhandlers(app):
