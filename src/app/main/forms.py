23df958dbef8e9398d479e7750e281830b4b3a08
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-06 19:47:54 -0500

    add UI Kit

--- src/app/main/forms.py
+++ src/app/main/forms.py
@@ -7,12 +7,15 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 from app import images
 
 
 class UploadForm(FlaskForm):
-    photo_description = StringField('Caption', validators=[DataRequired()])
-    photo = FileField('Photo Upload', validators=[FileAllowed(images, 'Image Only!'), FileRequired('Choose a file!')])
+    photo_description = StringField('Caption', validators=[DataRequired()],
+                                    widget=TextArea())
+    photo = FileField('Photo Upload', validators=[FileAllowed(images, 'Image Only!'),
+                                                  FileRequired('Choose a file!')])
     submit = SubmitField('Upload')
 
 class CommentForm(FlaskForm):
